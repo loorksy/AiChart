93714d40d9e69a10dcded82b166f4e42e3aa0810
loorksy <loorksy@gmail.com>
2026-06-11 21:32:06 +0300

    v22

--- infra/mt5/entrypoint.sh
+++ infra/mt5/entrypoint.sh
@@ -8,4 +8,4 @@ Xvfb :99 -screen 0 1280x800x16 &
 sleep 2
 
 echo "[mt5-bridge] starting REST shim on :18812"
-exec wine C:\\Python311\\python.exe Z:\\opt\\shim.py
+exec wine C:\\Python39\\python.exe Z:\\opt\\shim.py
